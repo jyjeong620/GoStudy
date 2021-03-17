87f805974f394443e2a49422eaa0b179c62ea42a
jyjeong620 <jyjeong620@naver.com>
2021-03-17 22:57:53 +0900

    fix: 함수명 변경

--- src/test.go
+++ src/test.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import "fmt"
 
+//import "fmt"
+//
+//type example1 struct {
+//	count int
+//}
+//type example2 struct {
+//	count int
+//	name  string
+//}
+//
+//func main() {
+//	var ex1 example1
+//	var ex2 example2
+//
+//	ex1 = example1(ex2)
+//
+//	fmt.Println(ex1)
+//}
+var count int
+
+func main() {
+	count = 10
+
+	plus(count)
+	fmt.Println(count)
+}
+
+func plus(count int) {
+	count++
+	fmt.Println(count)
 }
