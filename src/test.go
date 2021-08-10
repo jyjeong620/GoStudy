5ba364f8b9eb9a5cb69f66f05c5cfabd3bb841e6
JunYoungJeong <jeongjy@mrblue.com>
2021-08-10 22:10:08 +0900

    feat: 제한 예제

--- src/test.go
+++ src/test.go
@@ -1,35 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"sync"
+	"testing"
+	"time"
+)
 
-//import "fmt"
-//
-//type example1 struct {
-//	count int
-//}
-//type example2 struct {
-//	count int
-//	name  string
-//}
-//
-//func main() {
-//	var ex1 example1
-//	var ex2 example2
-//
-//	ex1 = example1(ex2)
-//
-//	fmt.Println(ex1)
-//}
-var count int
-
-func main() {
-	count = 10
-
-	plus(count)
-	fmt.Println(count)
+func connectToService() interface{} {
+	time.Sleep(1 * time.Second)
+	return struct{}{}
+}
+func startNetworkDaemon() *sync.WaitGroup {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		server, err := net.Listen("tcp", "localhost:8080")
+		if err != nil {
+			log.Fatalf("cannot listen: %v", err)
+		}
+		defer server.Close()
+		wg.Done()
+		for {
+			conn, err := server.Accept()
+			if err != nil {
+				log.Printf("cannot accept connection: %v", err)
+				continue
+			}
+			connectToService()
+			fmt.Fprintln(conn, "")
+			conn.Close()
+		}
+	}()
+	return &wg
 }
 
-func plus(count int) {
-	count++
-	fmt.Println(count)
+func init() {
+	daemonStarted := startNetworkDaemon()
+	daemonStarted.Wait()
+}
+
+func BenchmarkNetworkRequest(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		conn, err := net.Dial("tcp", "localhost:8080")
+		if err != nil {
+			b.Fatalf("cannot dial host: %v", err)
+		}
+
+		if _, err := ioutil.ReadAll(conn); err != nil {
+			b.Fatalf("cannot read: %v", err)
+		}
+		conn.Close()
+	}
 }
