3a9bb1c0fc3e163e6f00e91ef4cf11a8242a67d5
JunYoungJeong <jeongjy@mrblue.com>
2021-08-12 00:04:55 +0900

    feat: for select  예제

--- src/chapter_4/ForSelectLoop.go
+++ src/chapter_4/ForSelectLoop.go
@@ -1,18 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	done := make(chan interface{})
-	//stringStream := make(chan string)
 
-	for _, s := range []string{"a", "b", "c"} {
-		select {
-		case <-done:
-			fmt.Println("done")
-			return
-		default:
+	//done := make(chan interface{})
+	//stringStream := make(chan string)
+	//
+	//for _, s := range []string{"a", "b", "c"} {
+	//	fmt.Println(s)
+	//	select {
+	//	case <-done:
+	//		return
+	//	case stringStream <- s:
+	//		fmt.Println(s)
+	//	}
+	//}
+	//fmt.Println(stringStream)
+
+	//stringStream := make(chan string)
+	fmt.Println(1)
+	go func() {
+		done := make(chan interface{})
+		for _, s := range []string{"a", "b", "c"} {
+			select {
+			case <-done:
+				fmt.Println("done")
+				return
+			default:
+			}
+			fmt.Println(s)
 		}
-		fmt.Println(s)
-	}
+	}()
+	time.Sleep(1000)
 }
